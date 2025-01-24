e3577ae043ab4e43a5ccc49668af50f785481ac2
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2025-01-24 17:48:33 -0500

    added functionality to CLI options

--- src/birdsView.py
+++ src/birdsView.py
@@ -1,27 +1,59 @@
-import fileHandling
+import pandas as pd
+csv = pd.read_csv("src/bird_data.csv")
+
+def addBird():
+    print("bird added")
+
+def removeBird():
+    print("bird removed")
+
+def getBirdInfo(csv):
+    userChoice = input("Which bird would you like to know more about? ")
+    birdInfo = csv.loc[csv["name"] == userChoice]
+    if birdInfo.empty:
+        print("Sorry, that bird is not in our database.")
+    else:
+        print()
+        print(birdInfo.to_string(index=False))
+        print()
+
+def editBirdInfo(csv):
+    userChoice = input("Which bird would you like to edit? ")
+    birdInfo = csv.loc[csv["name"] == userChoice]
+    if birdInfo.empty:
+        print("Sorry, that bird is not in our database.")
+    else:
+        print()
+        print("Bird Edited!")
+        print()
+
 
 def cli():
-    option = 0
-    while option != -1:
+    while True:
         print("----------TernOps Bird App----------")
         print("|     0. Exit                      |")
         print("|     1. Add Bird                  |")
         print("|     2. Remove Bird               |")
         print("|     3. Show Birds                |")
         print("|     4. Bird Info                 |")
+        print("|     5. Edit Bird                 |")
         print("------------------------------------")
         option = int(input("Please enter your choice: "))
         
         if option == 0:
-            option = -1
-        elif option == 1:
-            print("Bird Added!")
+            break
+        #elif option == 1:
+        #    addBird()
         elif option == 2:
             print("Bird Removed!")
         elif option == 3:
-            print("Showing Birds!")
+            print(csv.name.to_string(index=False))
+            print()
         elif option == 4:
-            print("Which Bird would you like to learn more about? ")
+            getBirdInfo(csv)
+        elif option == 5:
+            editBirdInfo(csv)
+            
 
 if __name__ == "__main__":
     cli()
