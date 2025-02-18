8cac97b1cfdce2aa139118bcdae0e683897c037d
Ryan Coones <wrcoones@gmail.com>
2025-02-17 22:26:47 -0500

    reset repo and created some basic file structure to get started!

---
+++ src/accounts.py
@@ -0,0 +1,24 @@
+class Account:
+    def __init__(self, name: str, password: str, isAdmin: bool):
+        self.name = name
+        self.password = password
+        self.isAdmin = isAdmin
+    
+    def setName(name):
+        self.name = name
+    
+    def getName():
+        return self.name
+    
+    def setPassword(password):
+        self.password = password
+    
+    def getPassword():
+        return self.password
+    
+    def setIsAdmin(isAdmin):
+        self.isAdmin = isAdmin
+
+    def getIsAdmin():
+        return self.isAdmin
+
