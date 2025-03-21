a9a75379344f87efb66cd76acdba90e161e1d48d
Matteogeraldo05 <matteodeangelisgeraldo@gmail.com>
2025-03-21 16:18:05 -0400

    csv update

--- src/tests/test_accounts.py
+++ src/tests/test_accounts.py
@@ -1,7 +1,7 @@
 import accounts
 
 def test_register_user():
-    success, account, message = accounts.register_user("testUsername", "testPassword", False, "src\\Data\\accountInfo.csv")
+    success, account, message = accounts.register_user("testUsername", "testPassword", False, "src\\Test\\test_accountInfo.csv")
     assert success == True
     assert not isinstance(account, accounts.Guest)
     assert message != ""
